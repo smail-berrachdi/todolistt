3551535f784aa44dff184222f3663d2ed335df42
smail berrachedi <mimoumimou82@gmail.com>
2021-04-09 13:45:47 +0200

    first commit

---
+++ java script/index.js
@@ -0,0 +1,92 @@
+const ages =[35,30,25,17,18,22,16,2];
+const agesGreaterEighteen = ages ==> {
+    const agesGreaterEighteen18 =[];
+    for{const age of ages}  } {
+        if(age > 18)
+        {
+            agesGreaterEighteen18.push(age);
+        }
+    }
+    return agesGreaterEighteen18;
+alert(agesGreaterEighteen(ages));
+
+
+
+
+
+
+
+// function perimeterOfRectangle(length,width){
+// let resultat = (length*width);
+// console.log(resultat);
+// return resultat
+// }
+// perimeterOfRectangle(10,10)
+
+
+
+// function fullName (firstName,lastName){
+//     alert( firstName + " "+lastName);
+// }
+// fullName(" Smail","berrachdi");
+
+
+// function fullName (myname){
+//     alert("my name");
+// }
+// fullName(" smail berrachdi");
+
+
+// function surface(base,hauteur){
+//     let resultat = (base*hauteur)/2;
+//     return ,
+// }
+
+// var score = prompt("enter your score")
+// if(score>80 & score<100){
+//     alert(score + "congratulations you are in the grade A")
+// }else{  
+// if(score>70 & score<89){
+//     alert(score + " congratulations you are in the grade B")
+// }  }else {  
+// if(score>60 & score<69){
+//     alert(score + " congratulations you are in the grade C")
+// }   }else{ 
+// if(score>50 & score<59){
+//     alert(score + " congratulations you are in the grade D")
+// }  } else{
+// if(score>0 & score<49){
+//     alert(score + " congratulations you are in the grade F")
+// }     }  
+
+
+// var num1 = prompt("enter a number")
+// if(num1%2==0){
+//     alert(num1 + " is an even number")
+// }else{
+//     alert(num1 + " is an odd number")
+// }
+
+
+
+// 
+// var yourAge = prompt("Please enter your age", "");
+// let myAge=20
+// if(myAge>yourAge){
+// let differenceAge=myAge-yourAge;
+// alert("i am"+ differenceAge + "older than you");
+// }else{
+//     let differenceAge=yourAge-myAge;
+//     alert("you are" + differenceAge + "older than me" );
+// }
+
+
+
+
+// var a = prompt("enter A");
+// var b = prompt("enter B");
+// if(a > b){
+// alert("A is greater than B");
+// } else {
+// alert( "B is greater than A" );
+// }
